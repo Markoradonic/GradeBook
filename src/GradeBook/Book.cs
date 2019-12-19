b8d213782c493944f7b08ebd3bbd5876260a9a45
Marko Radonic <marko.radonic@outlook.com>
2019-12-19 04:00:29 +0100

    add method ShowStatistic from laptop

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -7,12 +8,31 @@ namespace GradeBook
     {
         public Book(string name) {
             grades = new List<double>();
-            this.name = name;
-        }
+            this.name = name; // sa this name kazemo da je to objekat i da name posle znaka jednako ide iz parametra funkcije 
+        } 
         
-        public void AddGrade (double grade) {
+        public void AddGrade (double grade) { // AddGrade je instanca member klase Book
             grades.Add(grade);
         }
+
+        public void ShowStatistic()
+        {
+            var result = 0.0;
+                var highGrade = double.MinValue;
+                var lowGrade = double.MaxValue;
+                foreach(var number in grades)
+                {   
+
+                   highGrade = Math.Max(number, highGrade);
+                   lowGrade = Math.Min(number, lowGrade);
+                    result += number;
+                }
+                result /= grades.Count;
+                Console.WriteLine($"The lowest grade is {lowGrade}");
+                Console.WriteLine($"The average grade is {result:N1}");// formatiramo na jednu decimalu
+                Console.WriteLine($"The higest grade is {highGrade}");
+        }
+
         // nije bas jasan deo nalazi se u tutorijalu classes and objects 
         private List<double> grades;
         private string name;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,17 +10,12 @@ namespace GradeBook
                 var book = new Book("Scott's grades book");
                 book.AddGrade(89.1);
                 book.AddGrade(90.1);
+                book.AddGrade(77.6);
+                book.ShowStatistic();
                 
             
-                var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
-                grades.Add(56.1);
-                var result = 0.0;
-                foreach(var number in grades)
-                {
-                    result += number;
-                }
-                result /= grades.Count;
-                Console.WriteLine($"The average grade is {result:N1}");// formatiramo na jednu decimalu
+                
+                
         }
     }
 }
