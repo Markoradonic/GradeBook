e7e01db95791c0eb0e7a90dbdf6c6eb1584a1aba
Marko Radonic <marko.radonic@outlook.com>
2019-12-20 00:40:08 +0100

    add ProjectReference

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact] // -> je atribut u C#-u
+        public void Test1()
+        {
+        var book = new Book("");
+
+        }
+    }
+}
